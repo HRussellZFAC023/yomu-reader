becf83eaeb22c80a6566c2217cfe42ccc9bcd449
Henry Russell <henry.russell@coop.co.uk>
2026-05-31 10:20:09 +0100

    build userscript and docs assets [skip ci]

--- docs/public/newtab/sw.js
+++ docs/public/newtab/sw.js
@@ -1,4 +1,4 @@
-const APP_HASH = '8ea603ba6349';
+const APP_HASH = '2bc3729e4bcb';
 const CACHE_NAME = `yomu-newtab-${APP_HASH}`;
 const SHELL = ['./', './index.html', './app.js', '../yomu.user.js'];
 
