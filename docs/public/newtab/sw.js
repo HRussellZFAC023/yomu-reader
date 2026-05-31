4ba2e49228931d2fd96e8d00c406ca592b02c61a
Henry Russell <henry.russell@coop.co.uk>
2026-05-31 03:14:29 +0100

    Refactor nested lookup and cache pruning
    (cherry picked from commit 5dd8473ce95471a7832b9d99c55be900e0dfead9)

--- docs/public/newtab/sw.js
+++ docs/public/newtab/sw.js
@@ -1,4 +1,4 @@
-const APP_HASH = '509a604c64d1';
+const APP_HASH = '733dd13f4714';
 const CACHE_NAME = `yomu-newtab-${APP_HASH}`;
 const SHELL = ['./', './index.html', './app.js', '../yomu.user.js'];
 
