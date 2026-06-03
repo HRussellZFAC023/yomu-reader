10e6f4ace89aa8b90d712b7966989ac989016e4e
Henry Russell <henry.russell@coop.co.uk>
2026-06-03 08:50:04 +0100

    Improve nonstandard Anki deck compatibility

--- docs/public/newtab/sw.js
+++ docs/public/newtab/sw.js
@@ -1,4 +1,4 @@
-const APP_HASH = '0ee97294d198';
+const APP_HASH = '4bee4bd119d4';
 const CACHE_NAME = `yomu-newtab-${APP_HASH}`;
 const SHELL = ['./', './index.html', './app.js', '../yomu.user.js'];
 
