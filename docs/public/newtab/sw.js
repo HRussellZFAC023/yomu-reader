16fdd3b0aa7612ea778cd0e91c76d68a165299d4
Henry Russell <henry.russell@coop.co.uk>
2026-06-10 15:03:17 +0100

    Release yomu reader 0.6.63

--- docs/public/newtab/sw.js
+++ docs/public/newtab/sw.js
@@ -1,4 +1,4 @@
-const APP_HASH = 'abcddd7bf138';
+const APP_HASH = '2e1e1c1d5514';
 const CACHE_NAME = `yomu-newtab-${APP_HASH}`;
 const SHELL = ['./', './index.html', './app.js', './styles.css', '../yomu.user.js'];
 const CACHEABLE_PATH_SUFFIXES = [
