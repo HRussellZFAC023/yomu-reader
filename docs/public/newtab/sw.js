f26b37f58ed80b1a731dc8b4368003c7f0696e2a
Henry Russell <22746105+HRussellZFAC023@users.noreply.github.com>
2026-06-12 15:10:42 +0100

    Release yomu reader 0.6.159

--- docs/public/newtab/sw.js
+++ docs/public/newtab/sw.js
@@ -1,4 +1,4 @@
-const APP_HASH = 'ad461effa05f';
+const APP_HASH = 'e54b069f166c';
 const CACHE_NAME = `yomu-newtab-${APP_HASH}`;
 const SHELL = ['./', './index.html', './app.js', './styles.css', '../yomu.user.js'];
 const CACHEABLE_PATH_SUFFIXES = [
