507a3e2dbc908584a5fb52745234f56ae00ed1c7
Henry Russell <22746105+HRussellZFAC023@users.noreply.github.com>
2026-06-14 10:48:51 +0100

    Finalize 0.6.198 release assets

--- docs/public/yomu.user.js
+++ docs/public/yomu.user.js
@@ -11891,6 +11891,7 @@ ${scopedInner}
     "stream finished",
     "no stream handler",
     ,
+    // determined by compression function
     "no callback",
     "invalid UTF-8 data",
     "extra field too long",
