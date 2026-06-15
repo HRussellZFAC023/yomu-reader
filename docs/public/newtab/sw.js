e42b47d7d443823e9f8ab4330b680c05fe22ef86
Henry Russell <22746105+HRussellZFAC023@users.noreply.github.com>
2026-06-15 10:35:51 +0100

    Release 0.7.37 newtab action safety

--- docs/public/newtab/sw.js
+++ docs/public/newtab/sw.js
@@ -1,4 +1,4 @@
-const APP_HASH = 'de1616fb9472';
+const APP_HASH = 'bdf6f079fed5';
 const CACHE_NAME = `yomu-newtab-${APP_HASH}`;
 const SHELL = ['./', './index.html', './app.js', './styles.css', '../yomu.user.js'];
 const CACHEABLE_PATH_SUFFIXES = [
--- docs/public/yomu.user.js
+++ docs/public/yomu.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         よむ
 // @namespace    https://github.com/HRussellZFAC023/yomu-reader
-// @version      0.7.36
+// @version      0.7.37
 // @author       Henry
 // @description  Japanese popup reader with JPDB, Jiten, Yomitan, OCR, subtitles, and Anki.
 // @license      MIT
