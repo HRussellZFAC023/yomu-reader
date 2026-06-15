dcdf5184878bb8a89f3b16dbadc649941fa1575f
Henry Russell <22746105+HRussellZFAC023@users.noreply.github.com>
2026-06-15 10:18:31 +0100

    Build release assets for 0.7.36

--- docs/public/newtab/sw.js
+++ docs/public/newtab/sw.js
@@ -1,4 +1,4 @@
-const APP_HASH = '9c21639ba7b1';
+const APP_HASH = 'de1616fb9472';
 const CACHE_NAME = `yomu-newtab-${APP_HASH}`;
 const SHELL = ['./', './index.html', './app.js', './styles.css', '../yomu.user.js'];
 const CACHEABLE_PATH_SUFFIXES = [
