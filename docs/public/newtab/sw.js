b679d223ce96f1fdc01de7ad5d2dafde40a43fbd
Henry Russell <22746105+HRussellZFAC023@users.noreply.github.com>
2026-06-27 11:11:29 +0100

    Build release assets for 1.4.144

--- docs/public/newtab/sw.js
+++ docs/public/newtab/sw.js
@@ -1,4 +1,4 @@
-const APP_HASH = 'ae276632c7c2';
+const APP_HASH = '253420b0a6c3';
 const CACHE_NAME = `yomu-newtab-${APP_HASH}`;
 const SHELL = ['./', './index.html', './manifest.webmanifest', './app.js', './styles.css', '../yomu.user.js'];
 const CACHEABLE_PATH_SUFFIXES = [
