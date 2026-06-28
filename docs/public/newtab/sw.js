6c7f9b8d2636b400ae777aeefa22ca04000a058d
Henry Russell <22746105+HRussellZFAC023@users.noreply.github.com>
2026-06-28 11:59:45 +0100

    Stabilize release check for Reddit locale fix

--- docs/public/newtab/sw.js
+++ docs/public/newtab/sw.js
@@ -1,4 +1,4 @@
-const APP_HASH = '5e4e4523d79a';
+const APP_HASH = '2b022ac562b7';
 const CACHE_NAME = `yomu-newtab-${APP_HASH}`;
 const SHELL = ['./', './index.html', './manifest.webmanifest', './app.js', './styles.css', '../yomu.user.js'];
 const CACHEABLE_PATH_SUFFIXES = [
