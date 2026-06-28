583c87151c4fe5061dbf62dd4828279290c909e0
Henry Russell <22746105+HRussellZFAC023@users.noreply.github.com>
2026-06-28 16:54:56 +0100

    Release 1.4.177 scanned PDF OCR repair

--- docs/public/newtab/sw.js
+++ docs/public/newtab/sw.js
@@ -1,4 +1,4 @@
-const APP_HASH = '1b5909350cf5';
+const APP_HASH = '9fef723cb8f7';
 const CACHE_NAME = `yomu-newtab-${APP_HASH}`;
 const SHELL = ['./', './index.html', './manifest.webmanifest', './app.js', './styles.css', '../yomu.user.js'];
 const CACHEABLE_PATH_SUFFIXES = [
