882b603602a4f785b28da028c88e015ca86a9277
Henry Russell <22746105+HRussellZFAC023@users.noreply.github.com>
2026-06-28 18:03:29 +0100

    Build Yomu Gaming release assets

--- docs/public/newtab/sw.js
+++ docs/public/newtab/sw.js
@@ -1,4 +1,4 @@
-const APP_HASH = '2f7592ecc543';
+const APP_HASH = 'f925ae3c4a03';
 const CACHE_NAME = `yomu-newtab-${APP_HASH}`;
 const SHELL = ['./', './index.html', './manifest.webmanifest', './app.js', './styles.css', '../yomu.user.js'];
 const CACHEABLE_PATH_SUFFIXES = [
