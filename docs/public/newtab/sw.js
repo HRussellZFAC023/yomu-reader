0327df6edc65fe8d71b71a555a21e048807bcb42
Henry Russell <22746105+HRussellZFAC023@users.noreply.github.com>
2026-06-28 12:01:48 +0100

    build: sync public assets after merge

--- docs/public/newtab/sw.js
+++ docs/public/newtab/sw.js
@@ -1,4 +1,4 @@
-const APP_HASH = '5e4e4523d79a';
+const APP_HASH = 'a39009c423c8';
 const CACHE_NAME = `yomu-newtab-${APP_HASH}`;
 const SHELL = ['./', './index.html', './manifest.webmanifest', './app.js', './styles.css', '../yomu.user.js'];
 const CACHEABLE_PATH_SUFFIXES = [
